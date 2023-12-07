1ed3513d19c97661994a33533e7678b0b3e5d3c9
ekdnsj <152577846+ekdnsj@users.noreply.github.com>
2023-12-07 10:17:43 +0900

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -27,33 +27,34 @@ $(function () {
     $(".text").eq(0).addClass("on");
   }, 200);
 
-  //input을 datepicker로 선언
-  $(".datepicker").datepicker({
-    dateFormat: "yy-mm-dd", //달력 날짜 형태
-    showOtherMonths: true, //빈 공간에 현재월의 앞뒤월의 날짜를 표시
-    showMonthAfterYear: true, // 월- 년 순서가아닌 년도 - 월 순서
-    changeYear: true, //option값 년 선택 가능
-    changeMonth: true, //option값 월 선택 가능
-    showOn: "both", //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시
-    buttonImage: "./img/icon_cal.png", //버튼 이미지 경로
-    buttonImageOnly: true, //버튼 이미지만 깔끔하게 보이게함
-    buttonText: "선택", //버튼 호버 텍스트
-    yearSuffix: "년", //달력의 년도 부분 뒤 텍스트
-    backgroundColor: "#fff",
+  $("#datepicker, #datepicker1").datepicker({
+    dateFormat: "yy-mm-dd", //Input Display Format 변경
+
+    showOtherMonths: true, //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+
+    showMonthAfterYear: true, //년도 먼저 나오고, 뒤에 월 표시
+
+    changeYear: true, //콤보박스에서 년 선택 가능
+
+    changeMonth: true, //콤보박스에서 월 선택 가능
+
+    yearSuffix: "년", //달력의 년도 부분 뒤에 붙는 텍스트
+
     monthNamesShort: [
-      "1월",
-      "2월",
-      "3월",
-      "4월",
-      "5월",
-      "6월",
-      "7월",
-      "8월",
-      "9월",
-      "10월",
-      "11월",
-      "12월",
-    ], //달력의 월 부분 텍스트
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+    ], //달력의 월 부분 텍스트
+
     monthNames: [
       "1월",
       "2월",
@@ -67,8 +68,9 @@ $(function () {
       "10월",
       "11월",
       "12월",
-    ], //달력의 월 부분 Tooltip
-    dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"], //달력의 요일 텍스트
+    ], //달력의 월 부분 Tooltip 텍스트
+
+    dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"], //달력의 요일 부분 텍스트
     dayNames: [
       "일요일",
       "월요일",
@@ -77,13 +79,12 @@ $(function () {
       "목요일",
       "금요일",
       "토요일",
-    ], //달력의 요일 Tooltip
-    minDate: "-5Y", //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
-    maxDate: "+5y", //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
-  });
-
-  //초기값을 오늘 날짜로 설정해줘야 합니다.
-  $("#datepicker").datepicker("setDate", "today"); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
+    ], //달력의 요일 부분 Tooltip 텍스트
+    minDate: "0M", //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+    maxDate: "+12M", //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+  }); //초기값을 오늘 날짜로 설정
+  $("#datepicker").datepicker("setDate", "today"); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
+  $("#datepicker1").datepicker("setDate", "today +3"); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
 
   /*스크롤효과*/
   let baseline = -800;
